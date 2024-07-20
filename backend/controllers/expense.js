0e3306eb9a860897461f625e77ceec7be8be2429
Alka Sah <alkasah485@gmail.com>
2024-07-20 08:42:17 -0700

    modification in expense controller

--- backend/controllers/expense.js
+++ backend/controllers/expense.js
@@ -6,5 +6,16 @@ module.exports = {
         Expense.find()
         .then((expenses) => res.json(expenses))
         .catch((err)=>res.status(500).json(err));
+    },
+
+    //add a Expense
+    addExpense(req,res) {
+        Expense.add(req.body)
+        .then((expense)=>res.json(expense))
+        .catch((err)=>{
+            console.log(err);
+            return res.status(500).json(err);
+        })
+
     }
 }
