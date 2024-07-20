8cf088d417767babf0fec557dbd7bc70838f7465
Alka Sah <alkasah485@gmail.com>
2024-07-20 08:58:53 -0700

    modification in expense controller

--- backend/controllers/expense.js
+++ backend/controllers/expense.js
@@ -7,8 +7,7 @@ module.exports = {
         .then((expenses) => res.json(expenses))
         .catch((err)=>res.status(500).json(err));
     },
-
-    //add a Expense
+   //add a Expense
     addExpense(req,res) {
         Expense.add(req.body)
         .then((expense)=>res.json(expense))
@@ -16,6 +15,15 @@ module.exports = {
             console.log(err);
             return res.status(500).json(err);
         })
-
+    },
+    //delete a Expense
+    deleteExpense(req,res) {
+        Expense.findOneAndDelete({_id:req.params.expenseid})
+        .then((expense) => 
+            !expense
+                ? res.status(404).json({message: 'No expense with this id'})
+                : res.json(expense) 
+        ).then(()=> res.json({message: 'Expense Deleted'}))
+         .catch((err) => res.status(500).json(err))
     }
 }
