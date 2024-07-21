f50d96a0d45d970a01ae48228511df9522acc592
Alka Sah <alkasah485@gmail.com>
2024-07-20 22:10:30 -0700

    added index.js

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const db = require('./config/connection');
 const PORT = process.env.PORT || 3005;
+const routes = require('./routes');
 
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(routes);
 
 
 db.once('open',() => {
