dc423c0d3dd26717d1f6a60971f167b335f2564f
Alka Sah <alkasah485@gmail.com>
2024-07-21 00:44:22 -0700

    modifications saved

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,41 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
-import './App.css'
+import styled from 'styled-components'
+import  bg from './img/bg.png';
+
+//import './App.css'
+
+const AppStyled  = styled.div`
+  height: 100vh;
+  background-image: url(${bg});
+  position:relative;
+  main{
+    flex: 1;
+    background: rgba(252, 246, 249, 0.78);
+    border: 3px solid #FFFFFF;
+    backdrop-filter: blur(4.5px);
+    border-radius: 32px;
+    overflow-x: hidden;
+    &::-webkit-scrollbar{
+      width: 0;
+    }
+  }
+  `
 
 function App() {
+
+  // const AppStyled  = styled.div`
+  // height: 100vh;
+  // background-image: white;
+  
+  // `
  
 
   return (
-    <>
+    <AppStyled className = "App">
+      <h1>Hello</h1>
       
-    </>
+    </AppStyled>
   )
 }
 
