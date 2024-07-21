ae1be3c641184ac7d307ac4fc6dd7d48f3dfb4e4
Alka Sah <alkasah485@gmail.com>
2024-07-21 02:11:09 -0700

    modification in useWindow

--- frontend/src/utils/useWindowSize.jsx
+++ frontend/src/utils/useWindowSize.jsx
@@ -1,9 +1,25 @@
 //import React from 'react'
+import { useState , useEffect} from "react"
 
 const useWindowSize = () => {
-  return (
-    <div>useWindowSize</div>
-  )
+
+    const [size , setSize] = useState([0, 0]);
+
+    useEffect(() => {
+        const updateSize = () => {
+            setSize([window.innerWidth, window.innerHeight])
+        }
+        window.addEventListener('resize' , updateSize)
+
+        return () => window.removeEventListener('resize' , updateSize)
+
+    },[])
+
+    return {
+        width: size[0],
+        height: size[1]
+    }
+
 }
 
 export default useWindowSize
